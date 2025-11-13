b4126d57371e00dee6b3a3f418f2934a6d15ee9b
hetspkn-code <het.spkn@gmail.com>
2025-11-13 23:26:00 +0530

    Create Reading.js

---
+++ models/Reading.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const gasSchema = new mongoose.Schema({
+  deviceId: { type: String, required: true, index: true },
+  value: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Reading', gasSchema);
